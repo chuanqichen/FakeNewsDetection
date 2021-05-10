621e1ee0e22ea2ced7ad0424304ad84468b84e80
Chuanqi Chen <chuanqi.chen@gmail.com>
2021-05-10 16:42:47 -0500

    Add Skip_train flag to load trainned model and go straight to prediction

--- BERT.py
+++ BERT.py
@@ -242,6 +242,7 @@ bert_optimizer = AdamW(bert_model.parameters(),
 # Number of training epochs. The BERT authors recommend between 2 and 4. 
 # We chose to run for 2,I have already seen that the model starts overfitting beyound 2 epochs
 epochs = 1
+skip_train = True
 
 # Total number of training steps is [number of batches] x [number of epochs]. 
 # (Note that this is not the same as the number of training samples).
@@ -268,215 +269,220 @@ bert_training_stats = []
 total_t0 = time.time()
 
 # For each epoch...
-for epoch_i in range(0, epochs):
-    
-    # ========================================
-    #               Training
-    # ========================================
-    
-    # Perform one full pass over the training set.
+if skip_train == False:
+    for epoch_i in range(0, epochs):
+
+        # ========================================
+        #               Training
+        # ========================================
+
+        # Perform one full pass over the training set.
+
+        print("")
+        print('======== Epoch {:} / {:} ========'.format(epoch_i + 1, epochs))
+        print('Training...')
+
+        # Measure how long the training epoch takes.
+        t0 = time.time()
+
+        # Reset the total loss for this epoch.
+        total_train_loss = 0
+
+        # Put the bert_model into training mode. Don't be mislead--the call to
+        # `train` just changes the *mode*, it doesn't *perform* the training.
+        # `dropout` and `batchnorm` layers behave differently during training
+        # vs. test (source: https://stackoverflow.com/questions/51433378/what-does-bert_model-train-do-in-pytorch)
+        bert_model.train()
+
+        # For each batch of training data...
+        for step, batch in enumerate(bert_train_dataloader):
+
+            # Progress update every 40 batches.
+            if step % 40 == 0 and not step == 0:
+                # Calculate elapsed time in minutes.
+                elapsed = format_time(time.time() - t0)
+
+                # Report progress.
+                print('  Batch {:>5,}  of  {:>5,}.    Elapsed: {:}.'.format(step, len(bert_train_dataloader), elapsed))
+
+            # Unpack this training batch from our dataloader.
+            #
+            # As we unpack the batch, we'll also copy each tensor to the GPU using the
+            # `to` method.
+            #
+            # `batch` contains three pytorch tensors:
+            #   [0]: input ids
+            #   [1]: attention masks
+            #   [2]: labels
+            b_input_ids = batch[0].to(device)
+            b_input_mask = batch[1].to(device)
+            b_labels = batch[2].to(device)
+
+            # Always clear any previously calculated gradients before performing a
+            # backward pass. PyTorch doesn't do this automatically because
+            # accumulating the gradients is "convenient while training RNNs".
+            # (source: https://stackoverflow.com/questions/48001598/why-do-we-need-to-call-zero-grad-in-pytorch)
+            bert_model.zero_grad()
+
+            # Perform a forward pass (evaluate the bert_model on this training batch).
+            # The documentation for this `bert_model` function is here:
+            # https://huggingface.co/transformers/v2.2.0/bert_model_doc/bert.html#transformers.BertForSequenceClassification
+            # It returns different numbers of parameters depending on what arguments
+            # are given and what flags are set. For our usage here, it returns
+            # the loss (because we provided labels) and the "logits"--the bert_model
+            # outputs prior to activation.
+            (loss, logits) = bert_model(b_input_ids,
+                                 token_type_ids=None,
+                                 attention_mask=b_input_mask,
+                                 labels=b_labels)
+
+            # Accumulate the training loss over all of the batches so that we can
+            # calculate the average loss at the end. `loss` is a Tensor containing a
+            # single value; the `.item()` function just returns the Python value
+            # from the tensor.
+            total_train_loss += loss.item()
+
+            # Perform a backward pass to calculate the gradients.
+            loss.backward()
+
+            # Clip the norm of the gradients to 1.0.
+            # This is to help prevent the "exploding gradients" problem.
+            torch.nn.utils.clip_grad_norm_(bert_model.parameters(), 1.0)
+
+            # Update parameters and take a step using the computed gradient.
+            # The bert_optimizer dictates the "update rule"--how the parameters are
+            # modified based on their gradients, the learning rate, etc.
+            bert_optimizer.step()
+
+            # Update the learning rate.
+            bert_scheduler.step()
+
+        # Calculate the average loss over all of the batches.
+        avg_train_loss = total_train_loss / len(bert_train_dataloader)
+
+        # Measure how long this epoch took.
+        training_time = format_time(time.time() - t0)
+
+        print("")
+        print("  Average training loss: {0:.2f}".format(avg_train_loss))
+        print("  Training epcoh took: {:}".format(training_time))
+
+        # ========================================
+        #               Validation
+        # ========================================
+        # After the completion of each training epoch, measure our performance on
+        # our validation set.
+
+        print("")
+        print("Running Validation...")
+
+        t0 = time.time()
+
+        # Put the bert_model in evaluation mode--the dropout layers behave differently
+        # during evaluation.
+        bert_model.eval()
+
+        # Tracking variables
+        total_eval_accuracy = 0
+        total_eval_loss = 0
+        nb_eval_steps = 0
+
+        batch_counter = 0
+        # Evaluate data for one epoch
+        for batch in bert_validation_dataloader:
+
+            # Unpack this training batch from our dataloader.
+            #
+            # As we unpack the batch, we'll also copy each tensor to the GPU using
+            # the `to` method.
+            #
+            # `batch` contains three pytorch tensors:
+            #   [0]: input ids
+            #   [1]: attention masks
+            #   [2]: labels
+            b_input_ids = batch[0].to(device)
+            b_input_mask = batch[1].to(device)
+            b_labels = batch[2].to(device)
+
+            # Tell pytorch not to bother with constructing the compute graph during
+            # the forward pass, since this is only needed for backprop (training).
+            with torch.no_grad():
+
+                # Forward pass, calculate logit predictions.
+                # token_type_ids is the same as the "segment ids", which
+                # differentiates sentence 1 and 2 in 2-sentence tasks.
+                # Get the "logits" output by the bert_model. The "logits" are the output
+                # values prior to applying an activation function like the softmax.
+                (loss, logits) = bert_model(b_input_ids,
+                                       token_type_ids=None,
+                                       attention_mask=b_input_mask,
+                                       labels=b_labels)
+
+            # Accumulate the validation loss.
+            total_eval_loss += loss.item()
+
+            # Move logits and labels to CPU
+            logits = logits.detach().cpu().numpy()
+            label_ids = b_labels.to('cpu').numpy()
+
+            # Calculate the accuracy for this batch of test sentences, and
+            # accumulate it over all batches.
+            total_eval_accuracy += flat_accuracy(logits, label_ids)
+
+
+        # Report the final accuracy for this validation run.
+        avg_val_accuracy = total_eval_accuracy / len(bert_validation_dataloader)
+        print("  Accuracy: {0:.2f}".format(avg_val_accuracy))
+
+        # Calculate the average loss over all of the batches.
+        avg_val_loss = total_eval_loss / len(bert_validation_dataloader)
+
+        # Measure how long the validation run took.
+        validation_time = format_time(time.time() - t0)
+
+        print("  Validation Loss: {0:.2f}".format(avg_val_loss))
+        print("  Validation took: {:}".format(validation_time))
+
+        # Record all statistics from this epoch.
+        bert_training_stats.append(
+            {
+                'epoch': epoch_i + 1,
+                'Training Loss': avg_train_loss,
+                'Valid. Loss': avg_val_loss,
+                'Valid. Accur.': avg_val_accuracy,
+                'Training Time': training_time,
+                'Validation Time': validation_time
+            }
+        )
 
     print("")
-    print('======== Epoch {:} / {:} ========'.format(epoch_i + 1, epochs))
-    print('Training...')
+    print("Training complete!")
 
-    # Measure how long the training epoch takes.
-    t0 = time.time()
+    print("Total training took {:} (h:mm:ss)".format(format_time(time.time()-total_t0)))
 
-    # Reset the total loss for this epoch.
-    total_train_loss = 0
+if skip_train:
+    bert_model = torch.load("bert_model_save")
+else:
+    torch.save(bert_model.state_dict(), "bert_model_save")
 
-    # Put the bert_model into training mode. Don't be mislead--the call to 
-    # `train` just changes the *mode*, it doesn't *perform* the training.
-    # `dropout` and `batchnorm` layers behave differently during training
-    # vs. test (source: https://stackoverflow.com/questions/51433378/what-does-bert_model-train-do-in-pytorch)
-    bert_model.train()
+    # open output file for writing
+    with open('bert_training_stats.txt', 'w') as filehandle:
+        json.dump(bert_training_stats, filehandle)
 
-    # For each batch of training data...
-    for step, batch in enumerate(bert_train_dataloader):
+    # Display floats with two decimal places.
+    pd.set_option('precision', 2)
 
-        # Progress update every 40 batches.
-        if step % 40 == 0 and not step == 0:
-            # Calculate elapsed time in minutes.
-            elapsed = format_time(time.time() - t0)
-            
-            # Report progress.
-            print('  Batch {:>5,}  of  {:>5,}.    Elapsed: {:}.'.format(step, len(bert_train_dataloader), elapsed))
+    # Create a DataFrame from our training statistics.
+    df_stats = pd.DataFrame(data=bert_training_stats)
 
-        # Unpack this training batch from our dataloader.
-        #
-        # As we unpack the batch, we'll also copy each tensor to the GPU using the 
-        # `to` method.
-        #
-        # `batch` contains three pytorch tensors:
-        #   [0]: input ids 
-        #   [1]: attention masks
-        #   [2]: labels 
-        b_input_ids = batch[0].to(device)
-        b_input_mask = batch[1].to(device)
-        b_labels = batch[2].to(device)
+    # Use the 'epoch' as the row index.
+    df_stats = df_stats.set_index('epoch')
 
-        # Always clear any previously calculated gradients before performing a
-        # backward pass. PyTorch doesn't do this automatically because 
-        # accumulating the gradients is "convenient while training RNNs". 
-        # (source: https://stackoverflow.com/questions/48001598/why-do-we-need-to-call-zero-grad-in-pytorch)
-        bert_model.zero_grad()        
+    # A hack to force the column headers to wrap.
+    #df = df.style.set_table_styles([dict(selector="th",props=[('max-width', '70px')])])
 
-        # Perform a forward pass (evaluate the bert_model on this training batch).
-        # The documentation for this `bert_model` function is here: 
-        # https://huggingface.co/transformers/v2.2.0/bert_model_doc/bert.html#transformers.BertForSequenceClassification
-        # It returns different numbers of parameters depending on what arguments
-        # are given and what flags are set. For our usage here, it returns
-        # the loss (because we provided labels) and the "logits"--the bert_model
-        # outputs prior to activation.
-        (loss, logits) = bert_model(b_input_ids, 
-                             token_type_ids=None, 
-                             attention_mask=b_input_mask, 
-                             labels=b_labels)
-
-        # Accumulate the training loss over all of the batches so that we can
-        # calculate the average loss at the end. `loss` is a Tensor containing a
-        # single value; the `.item()` function just returns the Python value 
-        # from the tensor.
-        total_train_loss += loss.item()
-
-        # Perform a backward pass to calculate the gradients.
-        loss.backward()
-
-        # Clip the norm of the gradients to 1.0.
-        # This is to help prevent the "exploding gradients" problem.
-        torch.nn.utils.clip_grad_norm_(bert_model.parameters(), 1.0)
-
-        # Update parameters and take a step using the computed gradient.
-        # The bert_optimizer dictates the "update rule"--how the parameters are
-        # modified based on their gradients, the learning rate, etc.
-        bert_optimizer.step()
-
-        # Update the learning rate.
-        bert_scheduler.step()
-
-    # Calculate the average loss over all of the batches.
-    avg_train_loss = total_train_loss / len(bert_train_dataloader)            
-    
-    # Measure how long this epoch took.
-    training_time = format_time(time.time() - t0)
-
-    print("")
-    print("  Average training loss: {0:.2f}".format(avg_train_loss))
-    print("  Training epcoh took: {:}".format(training_time))
-        
-    # ========================================
-    #               Validation
-    # ========================================
-    # After the completion of each training epoch, measure our performance on
-    # our validation set.
-
-    print("")
-    print("Running Validation...")
-
-    t0 = time.time()
-
-    # Put the bert_model in evaluation mode--the dropout layers behave differently
-    # during evaluation.
-    bert_model.eval()
-
-    # Tracking variables 
-    total_eval_accuracy = 0
-    total_eval_loss = 0
-    nb_eval_steps = 0
-
-    batch_counter = 0
-    # Evaluate data for one epoch
-    for batch in bert_validation_dataloader:
-
-        # Unpack this training batch from our dataloader. 
-        #
-        # As we unpack the batch, we'll also copy each tensor to the GPU using 
-        # the `to` method.
-        #
-        # `batch` contains three pytorch tensors:
-        #   [0]: input ids 
-        #   [1]: attention masks
-        #   [2]: labels 
-        b_input_ids = batch[0].to(device)
-        b_input_mask = batch[1].to(device)
-        b_labels = batch[2].to(device)
-        
-        # Tell pytorch not to bother with constructing the compute graph during
-        # the forward pass, since this is only needed for backprop (training).
-        with torch.no_grad():        
-
-            # Forward pass, calculate logit predictions.
-            # token_type_ids is the same as the "segment ids", which 
-            # differentiates sentence 1 and 2 in 2-sentence tasks.
-            # Get the "logits" output by the bert_model. The "logits" are the output
-            # values prior to applying an activation function like the softmax.
-            (loss, logits) = bert_model(b_input_ids, 
-                                   token_type_ids=None, 
-                                   attention_mask=b_input_mask,
-                                   labels=b_labels)
-            
-        # Accumulate the validation loss.
-        total_eval_loss += loss.item()
-
-        # Move logits and labels to CPU
-        logits = logits.detach().cpu().numpy()
-        label_ids = b_labels.to('cpu').numpy()
-
-        # Calculate the accuracy for this batch of test sentences, and
-        # accumulate it over all batches.
-        total_eval_accuracy += flat_accuracy(logits, label_ids)
-        
-
-    # Report the final accuracy for this validation run.
-    avg_val_accuracy = total_eval_accuracy / len(bert_validation_dataloader)
-    print("  Accuracy: {0:.2f}".format(avg_val_accuracy))
-
-    # Calculate the average loss over all of the batches.
-    avg_val_loss = total_eval_loss / len(bert_validation_dataloader)
-    
-    # Measure how long the validation run took.
-    validation_time = format_time(time.time() - t0)
-    
-    print("  Validation Loss: {0:.2f}".format(avg_val_loss))
-    print("  Validation took: {:}".format(validation_time))
-
-    # Record all statistics from this epoch.
-    bert_training_stats.append(
-        {
-            'epoch': epoch_i + 1,
-            'Training Loss': avg_train_loss,
-            'Valid. Loss': avg_val_loss,
-            'Valid. Accur.': avg_val_accuracy,
-            'Training Time': training_time,
-            'Validation Time': validation_time
-        }
-    )
-
-print("")
-print("Training complete!")
-
-print("Total training took {:} (h:mm:ss)".format(format_time(time.time()-total_t0)))
-
-torch.save(bert_model.state_dict(), "bert_model_save")
-# open output file for writing
-with open('bert_training_stats.txt', 'w') as filehandle:
-    json.dump(bert_training_stats, filehandle)
-
-# Display floats with two decimal places.
-pd.set_option('precision', 2)
-
-# Create a DataFrame from our training statistics.
-df_stats = pd.DataFrame(data=bert_training_stats)
-
-# Use the 'epoch' as the row index.
-df_stats = df_stats.set_index('epoch')
-
-# A hack to force the column headers to wrap.
-#df = df.style.set_table_styles([dict(selector="th",props=[('max-width', '70px')])])
-
-# Display the table.
-df_stats
+    # Display the table.
+    df_stats
 
 
 # Report the number of sentences in test dataset.
@@ -595,13 +601,13 @@ def get_eval_report(labels, preds):
 eval_report = get_eval_report(flat_true_labels, flat_predictions)
 print("eval summary: ", eval_report)
 
-with open('eval_report.txt', 'w') as filehandle2:
+with open('eval_report.json', 'w') as filehandle2:
     json.dump(eval_report, filehandle2)
 
 
 
 # The input data dir. Should contain the .tsv files (or other data files) for the task.
-DATA_DIR = "Data/"
+DATA_DIR = "../Data/"
 
 # Bert pre-trained model selected in the list: bert-base-uncased, 
 # bert-large-uncased, bert-base-cased, bert-large-cased, bert-base-multilingual-uncased,
