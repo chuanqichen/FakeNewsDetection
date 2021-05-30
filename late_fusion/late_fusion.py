a1ad5c57eb4e19984efb7354fd0eccebd9690320
Kaijun Feng <akahs0913@gmail.com>
2021-05-30 22:55:35 +0000

    Convert 3.5.1 model to 4.6.1 format

--- late_fusion/late_fusion.py
+++
@@ -1,8 +0,0 @@
-from transformers import BertForSequenceClassification, BertConfig
-from transformers.models.bert import modeling_bert
-import torch
-
-bert_model = BertForSequenceClassification(BertConfig())
-loaded = torch.load('../bert_model_save')
-bert_model.load_state_dict(loaded)
-
